089c875fcd1a305e74282070e32bf120c4c48687
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-03 14:01:57 -0400

    fix errors

--- primes.py
+++ primes.py
@@ -3,8 +3,17 @@
 
 def primes(number_of_primes):
     list = []
-    for n in range(1, number_of_primes + 1):
-        for i in range(2, n + 1):
-            if n % i != 0:
-                list.append(n)
+
+     n = 2
+    while len(list) != count:
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
+                break
+        else:
+            list.append(n)
+        n += 1
+    #for n in range(1, number_of_primes + 1):
+        #for i in range(2, n + 1):
+            #if n % i != 0:
+                #\list.append(n)
     return list
