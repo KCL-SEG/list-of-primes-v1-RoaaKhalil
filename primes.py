eb59e0189e0859bd873c1a8d073a071491abe6f4
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-03 14:32:43 -0400

    change solution

--- primes.py
+++ primes.py
@@ -12,8 +12,5 @@ def primes(number_of_primes):
         else:
             list.append(n)
         n += 1
-    #for n in range(1, number_of_primes + 1):
-        #for i in range(2, n + 1):
-            #if n % i != 0:
-                #\list.append(n)
+    
     return list
