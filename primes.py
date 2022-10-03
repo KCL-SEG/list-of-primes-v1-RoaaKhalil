4448ae9ebd40ab630650e50cf02c1ea23c8536a2
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-03 07:45:12 -0400

    add solution

--- primes.py
+++ primes.py
@@ -3,4 +3,8 @@
 
 def primes(number_of_primes):
     list = []
+    for n in range(1, number_of_primes):
+        for i in range(2, n):
+            if n % i != 0:
+                list.append(n)
     return list
