345ed030ce6fe4497bb9486d3aff7dbb8201bcc3
Roaa Khalil <rooaakhalil123@gmail.com>
2022-10-04 07:33:16 -0400

    change solution

--- primes.py
+++ primes.py
@@ -4,24 +4,20 @@
 def primes(number_of_primes):
     list = []
     num = 2
-    ctr = 2
-    list.append(2)
-    while ctr <= number_of_primes:
-        num = nextPrime(num)
-        list.append(num)
-        ctr +=1
+    ctr = 0
+
+    while ctr < number_of_primes:
+        prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                prime = False
+
+        if prime:
+            list.append(num)
+            ctr +=1
+            
+        num = num + 1
 
     return list
 
 
-def nextPrime(num):
-    while True:
-
-        num += 1
-
-        for i in range (2, num):
-            if num % i == 0:
-                break
-
-            else:
-                return num
